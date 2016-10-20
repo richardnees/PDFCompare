5f734fc406542e2c048cb27cfb90e9f663b2e601
Richard Nees <richard.nees@vimn.com>
2016-10-20 19:28:01 +0200

    Fix Typo

--- PDFCompare.playground/Contents.swift
+++ PDFCompare.playground/Contents.swift
@@ -5,10 +5,10 @@ import PDFCompare
 if
     let originalURL = Bundle.main.urlForImageResource("1"),
     let sameDataURL = Bundle.main.urlForImageResource("2"),
-    let differectDataURL = Bundle.main.urlForImageResource("3"),
+    let differentDataURL = Bundle.main.urlForImageResource("3"),
     let originalPDFDocument = PDFDocument(url: originalURL),
     let sameDataPDFDocument = PDFDocument(url: sameDataURL),
-    let differectDataPDFDocument = PDFDocument(url: differectDataURL) {
+    let differentDataPDFDocument = PDFDocument(url: differentDataURL) {
     
     let compare1and1Data = originalPDFDocument.compareData(to: originalPDFDocument)
     let compare1and1Metadata = originalPDFDocument.compareMetadata(to: originalPDFDocument)
@@ -16,6 +16,6 @@ if
     let compare1and2Data = originalPDFDocument.compareData(to: sameDataPDFDocument)
     let compare1and2Metadata = originalPDFDocument.compareMetadata(to: sameDataPDFDocument)
 
-    let compare1and3Data = originalPDFDocument.compareData(to: differectDataPDFDocument)
-    let compare1and3Metadata = originalPDFDocument.compareMetadata(to: differectDataPDFDocument)
+    let compare1and3Data = originalPDFDocument.compareData(to: differentDataPDFDocument)
+    let compare1and3Metadata = originalPDFDocument.compareMetadata(to: differentDataPDFDocument)
 }
